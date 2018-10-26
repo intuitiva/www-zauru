e116acc32838ff141d3898211d4c3d8b1c20f8f8
Luis Pedro Sarti <sartip@intuitiva.biz>
2018-10-25 23:11:41 -0600

    fix links

--- replacements.sh
+++ replacements.sh
@@ -8,43 +8,43 @@ sed -i '' 's/"index.html" class="footer-link w--current">P/"#Pricing" class="foo
 sed -i '' 's/"index.html" class="footer-link w--current">F/"#FAQ" class="footer-link w--current">F/g' static/home/index.html
 sed -i '' 's/"index.html" class="footer-link w--current">C/"#Contact" class="footer-link w--current">C/g' static/home/index.html
 sed -i '' 's/"blog.html"/"..\/blog"/g' static/home/index.html
-sed -i '' 's/"..\/index.html" class="nav-link w-nav-link w--current">M/"..\/index.html#Modules" class="nav-link w-nav-link w--current">M/g' static/home/industrias/desarrolladores-de-proyectos.html
-sed -i '' 's/"..\/index.html" class="nav-link w-nav-link w--current">P/"..\/index.html#Pricing" class="nav-link w-nav-link w--current">P/g' static/home/industrias/desarrolladores-de-proyectos.html
-sed -i '' 's/"..\/index.html" class="nav-link fixed-nav-link w-nav-link w--current">M/"..\/index.html#Modules" class="nav-link fixed-nav-link w-nav-link w--current">M/g' static/home/industrias/desarrolladores-de-proyectos.html
-sed -i '' 's/"..\/index.html" class="nav-link fixed-nav-link w-nav-link w--current">P/"..\/index.html#Pricing" class="nav-link fixed-nav-link w-nav-link w--current">P/g' static/home/industrias/desarrolladores-de-proyectos.html
-sed -i '' 's/"..\/index.html" class="footer-link w--current">I/"..\/index.html#Industries" class="footer-link w--current">I/g' static/home/industrias/desarrolladores-de-proyectos.html
-sed -i '' 's/"..\/index.html" class="footer-link w--current">M/"..\/index.html#Modules" class="footer-link w--current">M/g' static/home/industrias/desarrolladores-de-proyectos.html
-sed -i '' 's/"..\/index.html" class="footer-link w--current">P/"..\/index.html#Pricing" class="footer-link w--current">P/g' static/home/industrias/desarrolladores-de-proyectos.html
-sed -i '' 's/"..\/index.html" class="footer-link w--current">F/"..\/index.html#FAQ" class="footer-link w--current">F/g' static/home/industrias/desarrolladores-de-proyectos.html
-sed -i '' 's/"..\/index.html" class="footer-link w--current">C/"..\/index.html#Contact" class="footer-link w--current">C/g' static/home/industrias/desarrolladores-de-proyectos.html
+sed -i '' 's/"..\/index.html" class="nav-link w-nav-link">M/"..\/index.html#Modules" class="nav-link w-nav-link">M/g' static/home/industrias/desarrolladores-de-proyectos.html
+sed -i '' 's/"..\/index.html" class="nav-link w-nav-link">P/"..\/index.html#Pricing" class="nav-link w-nav-link">P/g' static/home/industrias/desarrolladores-de-proyectos.html
+sed -i '' 's/"..\/index.html" class="nav-link fixed-nav-link w-nav-link">M/"..\/index.html#Modules" class="nav-link fixed-nav-link w-nav-link">M/g' static/home/industrias/desarrolladores-de-proyectos.html
+sed -i '' 's/"..\/index.html" class="nav-link fixed-nav-link w-nav-link">P/"..\/index.html#Pricing" class="nav-link fixed-nav-link w-nav-link">P/g' static/home/industrias/desarrolladores-de-proyectos.html
+sed -i '' 's/"..\/index.html" class="footer-link">I/"..\/index.html#Industries" class="footer-link">I/g' static/home/industrias/desarrolladores-de-proyectos.html
+sed -i '' 's/"..\/index.html" class="footer-link">M/"..\/index.html#Modules" class="footer-link">M/g' static/home/industrias/desarrolladores-de-proyectos.html
+sed -i '' 's/"..\/index.html" class="footer-link">P/"..\/index.html#Pricing" class="footer-link">P/g' static/home/industrias/desarrolladores-de-proyectos.html
+sed -i '' 's/"..\/index.html" class="footer-link">F/"..\/index.html#FAQ" class="footer-link">F/g' static/home/industrias/desarrolladores-de-proyectos.html
+sed -i '' 's/"..\/index.html" class="footer-link">C/"..\/index.html#Contact" class="footer-link">C/g' static/home/industrias/desarrolladores-de-proyectos.html
 sed -i '' 's/"..\/blog.html"/"..\/..\/blog"/g' static/home/industrias/desarrolladores-de-proyectos.html
-sed -i '' 's/"..\/index.html" class="nav-link w-nav-link w--current">M/"..\/index.html#Modules" class="nav-link w-nav-link w--current">M/g' static/home/industrias/distribuidores.html
-sed -i '' 's/"..\/index.html" class="nav-link w-nav-link w--current">P/"..\/index.html#Pricing" class="nav-link w-nav-link w--current">P/g' static/home/industrias/distribuidores.html
-sed -i '' 's/"..\/index.html" class="nav-link fixed-nav-link w-nav-link w--current">M/"..\/index.html#Modules" class="nav-link fixed-nav-link w-nav-link w--current">M/g' static/home/industrias/distribuidores.html
-sed -i '' 's/"..\/index.html" class="nav-link fixed-nav-link w-nav-link w--current">P/"..\/index.html#Pricing" class="nav-link fixed-nav-link w-nav-link w--current">P/g' static/home/industrias/distribuidores.html
-sed -i '' 's/"..\/index.html" class="footer-link w--current">I/"..\/index.html#Industries" class="footer-link w--current">I/g' static/home/industrias/distribuidores.html
-sed -i '' 's/"..\/index.html" class="footer-link w--current">M/"..\/index.html#Modules" class="footer-link w--current">M/g' static/home/industrias/distribuidores.html
-sed -i '' 's/"..\/index.html" class="footer-link w--current">P/"..\/index.html#Pricing" class="footer-link w--current">P/g' static/home/industrias/distribuidores.html
-sed -i '' 's/"..\/index.html" class="footer-link w--current">F/"..\/index.html#FAQ" class="footer-link w--current">F/g' static/home/industrias/distribuidores.html
-sed -i '' 's/"..\/index.html" class="footer-link w--current">C/"..\/index.html#Contact" class="footer-link w--current">C/g' static/home/industrias/distribuidores.html
+sed -i '' 's/"..\/index.html" class="nav-link w-nav-link">M/"..\/index.html#Modules" class="nav-link w-nav-link">M/g' static/home/industrias/distribuidores.html
+sed -i '' 's/"..\/index.html" class="nav-link w-nav-link">P/"..\/index.html#Pricing" class="nav-link w-nav-link">P/g' static/home/industrias/distribuidores.html
+sed -i '' 's/"..\/index.html" class="nav-link fixed-nav-link w-nav-link">M/"..\/index.html#Modules" class="nav-link fixed-nav-link w-nav-link">M/g' static/home/industrias/distribuidores.html
+sed -i '' 's/"..\/index.html" class="nav-link fixed-nav-link w-nav-link">P/"..\/index.html#Pricing" class="nav-link fixed-nav-link w-nav-link">P/g' static/home/industrias/distribuidores.html
+sed -i '' 's/"..\/index.html" class="footer-link">I/"..\/index.html#Industries" class="footer-link">I/g' static/home/industrias/distribuidores.html
+sed -i '' 's/"..\/index.html" class="footer-link">M/"..\/index.html#Modules" class="footer-link">M/g' static/home/industrias/distribuidores.html
+sed -i '' 's/"..\/index.html" class="footer-link">P/"..\/index.html#Pricing" class="footer-link">P/g' static/home/industrias/distribuidores.html
+sed -i '' 's/"..\/index.html" class="footer-link">F/"..\/index.html#FAQ" class="footer-link">F/g' static/home/industrias/distribuidores.html
+sed -i '' 's/"..\/index.html" class="footer-link">C/"..\/index.html#Contact" class="footer-link">C/g' static/home/industrias/distribuidores.html
 sed -i '' 's/"..\/blog.html"/"..\/..\/blog"/g' static/home/industrias/distribuidores.html
-sed -i '' 's/"..\/index.html" class="nav-link w-nav-link w--current">M/"..\/index.html#Modules" class="nav-link w-nav-link w--current">M/g' static/home/industrias/empresas-con-cobros-recurrentes.html
-sed -i '' 's/"..\/index.html" class="nav-link w-nav-link w--current">P/"..\/index.html#Pricing" class="nav-link w-nav-link w--current">P/g' static/home/industrias/empresas-con-cobros-recurrentes.html
-sed -i '' 's/"..\/index.html" class="nav-link fixed-nav-link w-nav-link w--current">M/"..\/index.html#Modules" class="nav-link fixed-nav-link w-nav-link w--current">M/g' static/home/industrias/empresas-con-cobros-recurrentes.html
-sed -i '' 's/"..\/index.html" class="nav-link fixed-nav-link w-nav-link w--current">P/"..\/index.html#Pricing" class="nav-link fixed-nav-link w-nav-link w--current">P/g' static/home/industrias/empresas-con-cobros-recurrentes.html
-sed -i '' 's/"..\/index.html" class="footer-link w--current">I/"..\/index.html#Industries" class="footer-link w--current">I/g' static/home/industrias/empresas-con-cobros-recurrentes.html
-sed -i '' 's/"..\/index.html" class="footer-link w--current">M/"..\/index.html#Modules" class="footer-link w--current">M/g' static/home/industrias/empresas-con-cobros-recurrentes.html
-sed -i '' 's/"..\/index.html" class="footer-link w--current">P/"..\/index.html#Pricing" class="footer-link w--current">P/g' static/home/industrias/empresas-con-cobros-recurrentes.html
-sed -i '' 's/"..\/index.html" class="footer-link w--current">F/"..\/index.html#FAQ" class="footer-link w--current">F/g' static/home/industrias/empresas-con-cobros-recurrentes.html
-sed -i '' 's/"..\/index.html" class="footer-link w--current">C/"..\/index.html#Contact" class="footer-link w--current">C/g' static/home/industrias/empresas-con-cobros-recurrentes.html
+sed -i '' 's/"..\/index.html" class="nav-link w-nav-link">M/"..\/index.html#Modules" class="nav-link w-nav-link">M/g' static/home/industrias/empresas-con-cobros-recurrentes.html
+sed -i '' 's/"..\/index.html" class="nav-link w-nav-link">P/"..\/index.html#Pricing" class="nav-link w-nav-link">P/g' static/home/industrias/empresas-con-cobros-recurrentes.html
+sed -i '' 's/"..\/index.html" class="nav-link fixed-nav-link w-nav-link">M/"..\/index.html#Modules" class="nav-link fixed-nav-link w-nav-link">M/g' static/home/industrias/empresas-con-cobros-recurrentes.html
+sed -i '' 's/"..\/index.html" class="nav-link fixed-nav-link w-nav-link">P/"..\/index.html#Pricing" class="nav-link fixed-nav-link w-nav-link">P/g' static/home/industrias/empresas-con-cobros-recurrentes.html
+sed -i '' 's/"..\/index.html" class="footer-link">I/"..\/index.html#Industries" class="footer-link">I/g' static/home/industrias/empresas-con-cobros-recurrentes.html
+sed -i '' 's/"..\/index.html" class="footer-link">M/"..\/index.html#Modules" class="footer-link">M/g' static/home/industrias/empresas-con-cobros-recurrentes.html
+sed -i '' 's/"..\/index.html" class="footer-link">P/"..\/index.html#Pricing" class="footer-link">P/g' static/home/industrias/empresas-con-cobros-recurrentes.html
+sed -i '' 's/"..\/index.html" class="footer-link">F/"..\/index.html#FAQ" class="footer-link">F/g' static/home/industrias/empresas-con-cobros-recurrentes.html
+sed -i '' 's/"..\/index.html" class="footer-link">C/"..\/index.html#Contact" class="footer-link">C/g' static/home/industrias/empresas-con-cobros-recurrentes.html
 sed -i '' 's/"..\/blog.html"/"..\/..\/blog"/g' static/home/industrias/empresas-con-cobros-recurrentes.html
-sed -i '' 's/"..\/index.html" class="nav-link w-nav-link w--current">M/"..\/index.html#Modules" class="nav-link w-nav-link w--current">M/g' static/home/industrias/proveedores-de-servicios.html
-sed -i '' 's/"..\/index.html" class="nav-link w-nav-link w--current">P/"..\/index.html#Pricing" class="nav-link w-nav-link w--current">P/g' static/home/industrias/proveedores-de-servicios.html
-sed -i '' 's/"..\/index.html" class="nav-link fixed-nav-link w-nav-link w--current">M/"..\/index.html#Modules" class="nav-link fixed-nav-link w-nav-link w--current">M/g' static/home/industrias/proveedores-de-servicios.html
-sed -i '' 's/"..\/index.html" class="nav-link fixed-nav-link w-nav-link w--current">P/"..\/index.html#Pricing" class="nav-link fixed-nav-link w-nav-link w--current">P/g' static/home/industrias/proveedores-de-servicios.html
-sed -i '' 's/"..\/index.html" class="footer-link w--current">I/"..\/index.html#Industries" class="footer-link w--current">I/g' static/home/industrias/proveedores-de-servicios.html
-sed -i '' 's/"..\/index.html" class="footer-link w--current">M/"..\/index.html#Modules" class="footer-link w--current">M/g' static/home/industrias/proveedores-de-servicios.html
-sed -i '' 's/"..\/index.html" class="footer-link w--current">P/"..\/index.html#Pricing" class="footer-link w--current">P/g' static/home/industrias/proveedores-de-servicios.html
-sed -i '' 's/"..\/index.html" class="footer-link w--current">F/"..\/index.html#FAQ" class="footer-link w--current">F/g' static/home/industrias/proveedores-de-servicios.html
-sed -i '' 's/"..\/index.html" class="footer-link w--current">C/"..\/index.html#Contact" class="footer-link w--current">C/g' static/home/industrias/proveedores-de-servicios.html
+sed -i '' 's/"..\/index.html" class="nav-link w-nav-link">M/"..\/index.html#Modules" class="nav-link w-nav-link">M/g' static/home/industrias/proveedores-de-servicios.html
+sed -i '' 's/"..\/index.html" class="nav-link w-nav-link">P/"..\/index.html#Pricing" class="nav-link w-nav-link">P/g' static/home/industrias/proveedores-de-servicios.html
+sed -i '' 's/"..\/index.html" class="nav-link fixed-nav-link w-nav-link">M/"..\/index.html#Modules" class="nav-link fixed-nav-link w-nav-link">M/g' static/home/industrias/proveedores-de-servicios.html
+sed -i '' 's/"..\/index.html" class="nav-link fixed-nav-link w-nav-link">P/"..\/index.html#Pricing" class="nav-link fixed-nav-link w-nav-link">P/g' static/home/industrias/proveedores-de-servicios.html
+sed -i '' 's/"..\/index.html" class="footer-link">I/"..\/index.html#Industries" class="footer-link">I/g' static/home/industrias/proveedores-de-servicios.html
+sed -i '' 's/"..\/index.html" class="footer-link">M/"..\/index.html#Modules" class="footer-link">M/g' static/home/industrias/proveedores-de-servicios.html
+sed -i '' 's/"..\/index.html" class="footer-link">P/"..\/index.html#Pricing" class="footer-link">P/g' static/home/industrias/proveedores-de-servicios.html
+sed -i '' 's/"..\/index.html" class="footer-link">F/"..\/index.html#FAQ" class="footer-link">F/g' static/home/industrias/proveedores-de-servicios.html
+sed -i '' 's/"..\/index.html" class="footer-link">C/"..\/index.html#Contact" class="footer-link">C/g' static/home/industrias/proveedores-de-servicios.html
 sed -i '' 's/"..\/blog.html"/"..\/..\/blog"/g' static/home/industrias/proveedores-de-servicios.html
