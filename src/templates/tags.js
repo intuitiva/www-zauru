4496ef80de79a8c472b3e061e5e2a42006a2d54b
Luis Pedro Sarti <sartip@intuitiva.biz>
2018-10-28 22:51:41 -0600

    fix tag

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -33,7 +33,7 @@ class TagRoute extends React.Component {
                 <h3 className="title is-size-4 is-bold-light">{tagHeader}</h3>
                 <ul>{postLinks}</ul>
                 <p>
-                  <Link to="/tags/">Browse all tags</Link>
+                  <Link to="/tags/">Ver todas las etiquetas</Link>
                 </p>
               </div>
             </div>
