2b7214b70ce141a322f8cee60e2b93bf45cbc887
Luis Pedro Sarti <sartip@intuitiva.biz>
2018-10-29 12:19:45 -0600

    remove extra tabs

--- src/pages/tags/index.js
+++ src/pages/tags/index.js
@@ -13,27 +13,27 @@ const TagsPage = ({
 }) => (
   <Layout>
     <section className="hero" style={{ backgroundImage: `linear-gradient(180deg, rgba(34, 71, 101, .65), rgba(34, 71, 101, .65)), url(${Background})`, backgroundPosition: `0px 0px, 50% -94px`, backgroundRepeat: `repeat, no-repeat`, backgroundAttachment: `scroll, scroll`, backgroundSize: `auto, cover`}}>
-          <div className="hero-head">
-            <Navbar />
-          </div>
-          <div className="hero-body">
-            <div className="container has-text-centered">
-              <h1 className="title" style={{ color: `white`}}>Etiquetas</h1>
-            </div>
-          </div>
-          <div className="hero-foot hero-social">
-            <h6 className="hero-foot-header" style={{ color: `white`}}>Síguenos en</h6>
-            <div className="hero-foot-line"></div>
-            <div className="social-icons">
-              <a href="https://twitter.com/zauru_erp" target="_blank" rel="noopener noreferrer">
-                <img src={TwitterIcon} alt="Twitter" />
-                </a>
-              <a href="https://facebook.com/zauruerp" target="_blank" rel="noopener noreferrer">
-                <img src={FacebookIcon} alt="Facebook" />
-              </a>
-            </div>
-          </div>
-        </section>
+      <div className="hero-head">
+        <Navbar />
+      </div>
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1 className="title" style={{ color: `white`}}>Etiquetas</h1>
+        </div>
+      </div>
+      <div className="hero-foot hero-social">
+        <h6 className="hero-foot-header" style={{ color: `white`}}>Síguenos en</h6>
+        <div className="hero-foot-line"></div>
+        <div className="social-icons">
+          <a href="https://twitter.com/zauru_erp" target="_blank" rel="noopener noreferrer">
+            <img src={TwitterIcon} alt="Twitter" />
+            </a>
+          <a href="https://facebook.com/zauruerp" target="_blank" rel="noopener noreferrer">
+            <img src={FacebookIcon} alt="Facebook" />
+          </a>
+        </div>
+      </div>
+    </section>
     <section className="section">
       <Helmet title={`Etiquetas | ${title}`} />
       <div className="container content">
